e8fb2b1d5805ba7840ea96645594c29413edd133
Constantine Xipolitopoulos <constantinexipol@gmail.com>
2017-05-29 17:41:32 +0300

    added exporting to csv file functionality

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 from __future__ import print_function
 import sys
 from PIL import Image #python image library
-import numpy as np #numpy: scientific tools for python
-import matplotlib.pyplot as plt #plotting library for showing images
+import numpy as np #numpy: επιστημονικά εργαλεία
+import matplotlib.pyplot as plt #για να δείχνουμε εικόνες/γραφήματα
+import csv
 
-# Π2014116 Ξυπολιτόπουλος Κωνσταντίνος - Για την εργαστηριακή bonus εργασία 'Αναγνώρισης Προτύπων' 2016-2017
 
+# Π2014116 Ξυπολιτόπουλος Κωνσταντίνος - Για την εργαστηριακή bonus εργασία 'Αναγνώρισης Προτύπων' 2016-2017 (3 βαθμοί bonus)
+pixeldata = []
 firstresults = []
 Threshold = int(sys.argv[1]) #δυαδικό κατόφλι, οτιδήποτε κάτω απο αυτό γίνεται 0 (μάυρο), είναι το πρώτο argument στην κάλεση του προγράμματος
 t_Threshold = int(sys.argv[2]) #το συνολικό κατόφλι εικόνας για την κατάταξή της σε φωτεινή η σκοτεινή, είναι το δεύτερο argument
@@ -19,17 +21,21 @@ for i in range(1, 21):
     imgData = np.asarray(im) #η 'as_array' της numpy επιστρέφει την εικόνα που διάβασε η PIL σαν πίνακα (μονοδιάστατος γιατί είναι greyscale)
     binaryimage = (imgData > Threshold) * 1 #λογική πράξη Χ 1 δίνει την αναπαράστασή της σαν νούμερο.
 
-    plt.imshow(binaryimage)
-    plt.show()#δείξε την εικόνα που φορτώθηκε σε παράθηρο
+    #plt.imshow(binaryimage)
+    #plt.show()#δείξε την εικόνα που φορτώθηκε σε παράθηρο
 
     for row in binaryimage:
         for item in row:
             if item == 0:
                 shadowcount = shadowcount +1
-
+    pixeldata.append(shadowcount)
     print(shadowcount)
     if shadowcount > t_Threshold: firstresults.append(1) #αν έχει πολλά σκούρα σημεία, παίρνει μονάδα "1", δηλαδή είναι χαλασμένη
     else: firstresults.append(0) #αν δεν έχει πολλά σκούρα σημεία, παίρνει "0", δηλαδή δεν έχει πρόβλημα
 
+with open('result_data.csv', 'w') as file:
+    for es in range(1, 21):
+        s = "{},{},{}\n".format(es, pixeldata[es-1], firstresults[es-1])
+        file.write(s)
 
 print(firstresults)
